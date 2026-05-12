c41d4b175dbf30aa9d9e62f09d23073826cd70a8
guyera <thegraygrimm@graymansurvivaltech.com>
2026-05-12 14:36:22 -0400

    feat: route imports and route tree in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DecksPage from "./pages/DecksPage";
+import DeckDetailPage from "./pages/DeckDetailPage";
+import CollectionPage from "./pages/CollectionPage";
+import CardDetailPage from "./pages/CardDetailPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 const App = () => {
   return (
-    <h1>App Loaded</h1>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/decks" element={<ProtectedRoute><DecksPage /></ProtectedRoute>} />
+      <Route path="/decks/:deckId" element={<ProtectedRoute><DeckDetailPage /></ProtectedRoute>} />
+      <Route path="/collection" element={<ProtectedRoute><CollectionPage /></ProtectedRoute>} />
+      <Route path="/cards/:cardId" element={<ProtectedRoute><CardDetailPage /></ProtectedRoute>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
