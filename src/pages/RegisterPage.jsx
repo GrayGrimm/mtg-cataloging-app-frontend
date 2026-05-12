88a464a2f260484cd3fada39294c3af3b55789d5
guyera <thegraygrimm@graymansurvivaltech.com>
2026-05-12 15:43:33 -0400

    feat: implement RegisterPage with username/password fields and API integration

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import api from "../api/api.jsx";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
-  return <h1>Register Page</h1>;
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await api.post("/auth/register", {
+      username,
+      password,
+    });
+
+    navigate("/login");
+  };
+
+  return;
+  <div>
+    <h1>Register</h1>;
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Enter a Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        placeholder="Enter a password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </form>
+    <button type="submit">Create Account</button>
+  </div>;
 };
 
 export default RegisterPage;
